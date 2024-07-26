023acb229de597c81662f77fd7125a1ca0be3aed
rcorook <43137559+rcorook@users.noreply.github.com>
2024-07-26 09:57:16 +0900

    Add updated RootRoutes

--- src/RootRoutes.js
+++ src/RootRoutes.js
@@ -2,10 +2,12 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Layout/Home";
 import NotFound from "./Layout/NotFound";
-import CreateEditDeck from "./Layout/CreateEditDeck";
+import CreateDeck from "./Layout/CreateDeck";
+import EditDeck from "./Layout/EditDeck";
 import Deck from "./Layout/Deck";
 import Study from "./Layout/Study";
-import AddEditCard from "./Layout/AddEditCard";
+import AddCard from "./Layout/AddCard";
+import EditCard from "./Layout/EditCard";
 import DeckProvider from "./Layout/DeckProvider";
 
 function RootRoutes() {
@@ -14,12 +16,12 @@ function RootRoutes() {
             <Route path="/" element={<Home />} />
             <Route path="*" element={<NotFound/>}/>
             <Route element={<DeckProvider/>}>
-                <Route path="/decks/new" element={<CreateEditDeck />}/>
-                <Route path="/decks/:deckId" element={<Deck />}/> {/*View button*/}
-                <Route path="/decks/:deckId/edit" element={<CreateEditDeck />}/>
-                <Route path="/decks/:deckId/study" element={<Study />} /> {/* Study button*/}
-                <Route path="/decks/:deckId/cards/new" element={<AddEditCard />}/>
-                <Route path="/decks/:deckId/cards/:cardId/edit" element={<AddEditCard />}/>
+                <Route path="/decks/new" element={<CreateDeck />}/>
+                <Route path="/decks/:deckId" element={<Deck />}/> {/*Entry point: View button*/}
+                <Route path="/decks/:deckId/edit" element={<EditDeck />}/>
+                <Route path="/decks/:deckId/study" element={<Study />} />
+                <Route path="/decks/:deckId/cards/new" element={<AddCard />}/>
+                <Route path="/decks/:deckId/cards/:cardId/edit" element={<EditCard />}/>
             </Route>
         </Routes>
     )
